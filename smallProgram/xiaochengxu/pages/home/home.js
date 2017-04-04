76834320461323966b28e9bc321139433e256643
MarkChung90 <zhong_zhq@hisuntech.com>
2017-04-04 23:21:23 +0800

    增加注释

---
+++ smallProgram/xiaochengxu/pages/home/home.js
@@ -0,0 +1,88 @@
+//首页
+var WatchJS = require("../../utils/watch.js")
+var watch = WatchJS.watch;
+var unwatch = WatchJS.unwatch;
+var callWatchers = WatchJS.callWatchers;
+var app = getApp();
+
+Page({
+
+  data: {
+    userInfo: {},
+    shop: {},
+  },
+
+  //去看看
+  tapEnterShop: function() {
+    console.log('go navigation itemList')
+    wx.navigateTo({
+      url: '../itemList/itemList'
+      // success: function (e) {  
+      //   var page = getCurrentPages().pop();  
+      //   if (page == undefined || page == null) return;  
+      //   page.onShow();  
+      // }
+    })
+  },
+
+  //我的订单
+  tapCheckOrderList: function() {
+    console.log('go navigation')
+    wx.navigateTo({
+      url: '../orderList/index'
+      // success: function (e) {  
+      //   var page = getCurrentPages().pop();  
+      //   if (page == undefined || page == null) return;  
+      //   page.onShow();  
+      // }
+    })
+  },
+  
+  //????
+  getAttentionShopList: function () {
+    wx.request({
+        url: app.globalData.serverHost + '/api/user/query',
+        data: {
+                openid : app.globalData.userOpenID,
+                token : app.globalData.session_key
+              },
+        method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+        //header: {
+        //  'content-type': 'application/json'
+        //},
+        success: function(res){
+          // success
+          console.log("/api/user/query success");
+          console.log(res.data);
+        },
+        fail: function(res) {
+          // fail
+          console.log("/api/user/query fail");
+          console.log(res);
+        },
+        complete: function() {
+          // complete
+        }
+    })
+  },
+
+  onLoad: function () {
+    console.log('onLoad')
+    console.log(app.globalData.userOpenID)
+    console.log(app.globalData.session_key)
+
+    var that = this
+
+    watch(app.globalData, "hasRegistered", function(){
+      console.log("hasRegistered changed!");
+      console.log(app.globalData.userOpenID);
+      that.getAttentionShopList();
+    });
+
+    if(app.globalData.hasRegistered) {
+      this.getAttentionShopList();
+    }
+
+  }
+
+})
