7ea1af2223150788bbd4771e52918de5545b4da5
xiemin <xiemin@xiemindeMacBook-Air.local>
2017-04-18 09:30:05 +0800

    parse string detail to json

--- xiaochengxu/pages/orderList/index.js
+++ xiaochengxu/pages/orderList/index.js
@@ -3,7 +3,7 @@ Page({
   data: {
     totalprice: 0,
     orderno: null,
-    currentpage: 0,
+    currentpage: 1,
     orderList: []
   },
 
@@ -18,15 +18,23 @@ Page({
                 openid : app.globalData.userOpenID,
                 token : app.globalData.session_key,
                 pageno : that.data.currentpage,
-                pagesize : 10
+                pagesize : 10,
+                date: '20170415'
               },
       method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       // header: {}, // 设置请求的 header
       success: function(res) {
         // success
         console.log(res)
+        var orderList = res.data.orderlist || [];
+        for(var i = 0, len = orderList.length; i < len; i++) {
+          if(typeof orderList[i].detail === 'string') {
+            orderList[i].detail = JSON.parse(orderList[i].detail);  
+          }         
+          console.log(orderList[i].detail)
+        }
         that.setData({
-          orderList : res.data.orderlist
+          orderList : orderList
         })
       },
       fail: function() {
